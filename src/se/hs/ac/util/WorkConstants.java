cfe9dfccdddcf45e5d439edc0c39897de4d5ecf0
jeonyeonbin <qtrgor@gmail.com>
2021-01-01 13:36:57 +0900

    feat: 상수 및 실행
    - 상수 선언
    - life cycle에 의해 run method로 첫시작

---
+++ src/se/hs/ac/util/WorkConstants.java
@@ -0,0 +1,17 @@
+package se.hs.ac.util;
+
+import java.time.format.DateTimeFormatter;
+
+public class WorkConstants {
+
+	public static final DateTimeFormatter YYYYMMDDHHMMSS = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+	public static class Menu {
+		public static final String ADD = "1";
+		public static final String REMOVE = "2";
+		public static final String UPDATE = "3";
+		public static final String SHOW = "4";
+		public static final String EXIT = "5";
+	}
+
+}
